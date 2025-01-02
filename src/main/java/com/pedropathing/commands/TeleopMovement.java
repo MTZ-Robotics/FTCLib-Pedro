9d2f1bd7de335a3dc0d7907e8d34216d781e2723
baronclaps <barbarhendo180@gmail.com>
2025-01-02 15:37:21 -0600

    add teleopmovement functionality

--- src/main/java/com/pedropathing/commands/TeleopMovement.java
+++ src/main/java/com/pedropathing/commands/TeleopMovement.java
@@ -2,6 +2,7 @@ package com.pedropathing.commands;
 
 import com.arcrobotics.ftclib.command.CommandBase;
 
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.pathgen.Path;
@@ -9,20 +10,76 @@ import com.pedropathing.pathgen.Path;
 
 public class TeleopMovement extends CommandBase {
     private final Follower follower;
+    private GamepadEx gamepad;
+    private double speed = 1;
+    private int leftYDirection = 1, leftXDirection = 1, rightXDirection = 1;
+    private boolean robotCentric = true;
 
-    public TeleopMovement(Follower follower) {
+    public TeleopMovement(Follower follower, boolean robotCentric, GamepadEx gamepad, double speed, int forwardMovementDirection, int lateralMovementDirection, int angularMovementDirection) {
         this.follower = follower;
+        this.robotCentric = robotCentric;
+        this.gamepad = gamepad;
+        this.speed = speed;
+        this.leftYDirection = forwardMovementDirection;
+        this.leftXDirection = lateralMovementDirection;
+        this.rightXDirection = angularMovementDirection;
+    }
+
+    public TeleopMovement(Follower follower, boolean robotCentric, GamepadEx gamepad, double speed) {
+        this(follower, robotCentric, gamepad, speed, 1, 1, 1);
+    }
+
+    public TeleopMovement(Follower follower, boolean robotCentric, GamepadEx gamepad) {
+        this(follower, robotCentric, gamepad, 1, 1,1,1);
+    }
+
+    public TeleopMovement(Follower follower, GamepadEx gamepad, double speed) {
+        this(follower,true, gamepad, speed, 1, 1, 1);
+    }
+
+    public TeleopMovement(Follower follower, boolean robotCentric, GamepadEx gamepad, int forwardMovementDirection, int lateralMovementDirection, int angularMovementDirection) {
+        this(follower, robotCentric, gamepad, 1, forwardMovementDirection, lateralMovementDirection, angularMovementDirection);
+    }
+
+    public TeleopMovement(Follower follower, GamepadEx gamepad, double speed, int forwardMovementDirection, int lateralMovementDirection, int angularMovementDirection) {
+        this(follower,true, gamepad, speed, forwardMovementDirection, lateralMovementDirection, angularMovementDirection);
+    }
+
+    public TeleopMovement setJoystickDirections(int forwardMovementDirection, int lateralMovementDirection, int angularMovementDirection) {
+        this.leftYDirection = forwardMovementDirection;
+        this.leftXDirection = lateralMovementDirection;
+        this.rightXDirection = angularMovementDirection;
+        return this;
+    }
+
+    public TeleopMovement setRobotCentric() {
+        this.robotCentric = true;
+        return this;
+    }
+
+    public TeleopMovement setFieldCentric() {
+        this.robotCentric = false;
+        return this;
+    }
+
+    public TeleopMovement setSpeed(double speed) {
+        this.speed = speed;
+        return this;
     }
 
     @Override
     public void initialize() {
+        follower.startTeleopDrive();
     }
 
     @Override
-    public boolean isFinished() {
+    public void execute() {
+        follower.setTeleOpMovementVectors(leftYDirection * speed * gamepad.getLeftY(), leftXDirection * speed * gamepad.getLeftX(), rightXDirection * speed * gamepad.getRightX(), robotCentric);
     }
 
     @Override
     public void end(boolean interrupted) {
+        follower.breakFollowing();
     }
 }
+
